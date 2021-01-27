f2cd4370cf44a6397acd161717ab74cdc628c721
Arne Keller <arne.keller@student.kit.edu>
2021-01-27 21:04:40 +0100

    AbsTerm, AppTerm, mehr Parser-Tests

--- src/main/java/edu/kit/typicalc/model/term/AbsTerm.java
+++ src/main/java/edu/kit/typicalc/model/term/AbsTerm.java
@@ -1,7 +1,38 @@
 package edu.kit.typicalc.model.term;
 
+import java.util.Objects;
+
 public class AbsTerm extends LambdaTerm {
+	private final VarTerm var;
+	private final LambdaTerm body;
+
 	public AbsTerm(VarTerm var, LambdaTerm body) {
-		// TODO
+		this.var = var;
+		this.body = body;
+	}
+
+	public VarTerm getVariable() {
+		return var;
+	}
+
+	public LambdaTerm getInner() {
+		return body;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		AbsTerm absTerm = (AbsTerm) o;
+		return Objects.equals(var, absTerm.var) && Objects.equals(body, absTerm.body);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(var, body);
 	}
 }
--- src/test/java/edu/kit/typicalc/model/parser/LambdaParserTest.java
+++ src/test/java/edu/kit/typicalc/model/parser/LambdaParserTest.java
@@ -2,6 +2,8 @@ package edu.kit.typicalc.model.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import edu.kit.typicalc.model.term.AbsTerm;
+import edu.kit.typicalc.model.term.AppTerm;
 import edu.kit.typicalc.model.term.VarTerm;
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +12,17 @@ class LambdaParserTest {
 	void varTerm() {
 		LambdaParser parser = new LambdaParser("x");
 		assertEquals(parser.parse().unwrap(), new VarTerm("x"));
+	}
+	@Test
+	void absTerm() {
+		LambdaParser parser = new LambdaParser("λx.x");
+		assertEquals(parser.parse().unwrap(), new AbsTerm(new VarTerm("x"), new VarTerm("x")));
+	}
+	@Test
+	void appTerm() {
+		LambdaParser parser = new LambdaParser("(λx.x)(λx.x)");
+		assertEquals(parser.parse().unwrap(),
+				new AppTerm(new AbsTerm(new VarTerm("x"), new VarTerm("x")),
+						new AbsTerm(new VarTerm("x"), new VarTerm("x"))));
 	}
 }
