3d5c84be033a0e9b4bb1d59a68b6e3b393e5ee41
Arne Keller <arne.keller@student.kit.edu>
2021-01-27 10:23:54 +0100

    Erster Unittest für Parser

--- src/main/java/edu/kit/typicalc/model/term/VarTerm.java
+++ src/main/java/edu/kit/typicalc/model/term/VarTerm.java
@@ -1,7 +1,25 @@
 package edu.kit.typicalc.model.term;
 
+import java.util.Objects;
+
 public class VarTerm extends LambdaTerm {
-	public VarTerm(String s) {
+	private final String name;
+
+	public VarTerm(String name) {
 		super();
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		VarTerm varTerm = (VarTerm) o;
+		return Objects.equals(name, varTerm.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
 	}
 }
