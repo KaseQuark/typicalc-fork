451c99bb2d403491620fc57b4a678bfcfbe06b44
Johanna Stuber <johanna.stuber@gmx.net>
2021-01-28 10:12:22 +0100

    Tree visit(appTerm)

--- src/main/java/edu/kit/typicalc/model/parser/ParseError.java
+++ src/main/java/edu/kit/typicalc/model/parser/ParseError.java
@@ -1,7 +1,16 @@
 package edu.kit.typicalc.model.parser;
 
 public enum ParseError {
+
+    /**
+     *  the lambda term didn't meet the specified syntax
+     */
     UNEXPECTED_TOKEN,
+    
     TOO_MANY_TOKENS,
+
+    /**
+     * the String contained a character not allowed in that place
+     */
     UNEXPECTED_CHARACTER
 }
--- src/main/java/edu/kit/typicalc/model/type/FunctionType.java
+++ src/main/java/edu/kit/typicalc/model/type/FunctionType.java
@@ -1,5 +1,7 @@
 package edu.kit.typicalc.model.type;
 
 public class FunctionType extends Type {
+    public FunctionType(Type parameter, Type output) {
+    }
 
 }
