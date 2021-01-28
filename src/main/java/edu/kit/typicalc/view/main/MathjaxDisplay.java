9f13bb7e4218fa595ae87c9063d0e3b119cc8bbc
Arne Keller <arne.keller@student.kit.edu>
2021-01-28 16:18:42 +0100

    MathJax without Shadow DOM (did not work)
    Checkstyle excludes
    Bundle MathJax

--- src/main/java/edu/kit/typicalc/view/main/MathjaxDisplay.java
+++ src/main/java/edu/kit/typicalc/view/main/MathjaxDisplay.java
@@ -1,6 +1,7 @@
 package edu.kit.typicalc.view.main;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.littemplate.LitTemplate;
@@ -8,6 +9,7 @@ import com.vaadin.flow.component.template.Id;
 
 @Tag("tc-display")
 @JsModule("./src/mathjax-display.ts")
+@JavaScript("./src/tex-svg-full.js")
 public class MathjaxDisplay extends LitTemplate {
 
     @Id("tc-content")
