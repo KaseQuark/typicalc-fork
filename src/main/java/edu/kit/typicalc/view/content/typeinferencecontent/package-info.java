c24ef80250ca23b4f0589571c385973cba20c1b3
Me <th.m.heinen@gmail.com>
2021-01-30 13:25:03 +0100

    implement visitType methods in LatexCreator

---
+++ src/main/java/edu/kit/typicalc/view/content/typeinferencecontent/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package contains the needed classes for displaying the type inference algorithm step-by-step.
+ */
+@NonNullFields
+@NonNullApi
+package edu.kit.typicalc.view.content.typeinferencecontent;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
