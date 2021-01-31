c1cf786d3f4e4ae2fb1f0975419b7a74e1cab6f1
Moritz Dieing <63721811+moritzdieing@users.noreply.github.com>
2021-02-01 00:31:50 +0100

    package info for view and view.main

---
+++ src/main/java/edu/kit/typicalc/view/main/package-info.java
@@ -0,0 +1,10 @@
+/** 
+ * This package contains classes for building the constantly displayed parts of the UI. This includes classes
+ * which provide methods to pass the user input to the {@link edu.kit.typicalc.presenter.Presenter}.
+ */
+@NonNullApi
+@NonNullFields
+package edu.kit.typicalc.view.main;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
---
+++ src/main/java/edu/kit/typicalc/view/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package contains all the classes needed to build the user interface. This includes classes, which
+ * take the user input and pass it to the {@link edu.kit.typicalc.presenter.Presenter} as well as classes
+ * to display the result of the type inference algorithm.
+ */
+@NonNullApi
+@NonNullFields
+package edu.kit.typicalc.view;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
