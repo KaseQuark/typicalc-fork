70c4f5ce430d0534bafb911860b373d8b53a9726
ucrhh <ucrhh@student.kit.edu>
2021-02-03 10:40:23 +0100

    read lambda term from url

--- src/main/java/edu/kit/typicalc/view/main/MainViewImpl.java
+++ src/main/java/edu/kit/typicalc/view/main/MainViewImpl.java
@@ -6,12 +6,17 @@ import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.router.Location;
+import com.vaadin.flow.router.*;
 import edu.kit.typicalc.model.ModelImpl;
 import edu.kit.typicalc.model.TypeInfererInterface;
 import edu.kit.typicalc.model.parser.ParseError;
 import edu.kit.typicalc.presenter.Presenter;
+import edu.kit.typicalc.view.content.infocontent.StartPageView;
 import edu.kit.typicalc.view.content.typeinferencecontent.TypeInferenceView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * Contains all the displayed components and builds the applications user interface (UI).
@@ -23,19 +28,22 @@ import edu.kit.typicalc.view.content.typeinferencecontent.TypeInferenceView;
 @JsModule("./styles/shared-styles.js")
 @JavaScript("./src/svg-pan-zoom.min.js")
 @JavaScript("./src/tex-svg-full.js")
-public class MainViewImpl extends AppLayout implements MainView {
+public class MainViewImpl extends AppLayout implements MainView, HasErrorParameter<NotFoundException> {
     private static final long serialVersionUID = -2411433187835906976L;
     private static final String ROUTE = "infer";
     private static final String SLASH = "/";
 
-    private String termToType = "lambda x.x"; //todo replace with real value
+    private final UpperBar upperBar;
+
+    private String termToType = "x"; //todo replace with real value
     /**
      * Creates a new MainViewImpl.
      */
     public MainViewImpl() {
         setDrawerOpened(false);
         MainViewListener presenter = new Presenter(new ModelImpl(), this);
-        addToNavbar(true, new UpperBar(presenter, this::setContent));
+        upperBar = new UpperBar(presenter, this::setContent);
+        addToNavbar(upperBar);
         addToDrawer(new DrawerContent());
     }
 
@@ -51,5 +59,32 @@ public class MainViewImpl extends AppLayout implements MainView {
         //TODO add error keys to bundle
         final Notification errorNotification = new ErrorNotification(getTranslation("root." + error.toString()));
         errorNotification.open();
+    }
+
+    @Override
+    public int setErrorParameter(
+            BeforeEnterEvent event,
+            ErrorParameter<NotFoundException> parameter) {
+
+        if (event.getLocation().getPath().matches(ROUTE + SLASH + ".*")) {
+            List<String> segments = event.getLocation().getSegments();
+            String term = segments.get(segments.size() - 1);
+            upperBar.inferTerm(decodeURL(term));
+            return HttpServletResponse.SC_OK;
+        } else if (event.getLocation().getPath().equals("")) {
+            setContent(new StartPageView());
+            return HttpServletResponse.SC_OK;
+        } else {
+            setContent(new NotFoundView(event));
+            return HttpServletResponse.SC_NOT_FOUND;
+        }
+    }
+
+    private String decodeURL(String encodedUrl) {
+        try {
+            return java.net.URLDecoder.decode(encodedUrl, StandardCharsets.UTF_8);
+        } catch (IllegalArgumentException e) {
+            return "";
+        }
     }
 }
