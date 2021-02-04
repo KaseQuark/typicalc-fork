cab1e7449001a1c15a7c57cbd2793bc7fd13a6e3
Moritz Dieing <63721811+moritzdieing@users.noreply.github.com>
2021-02-04 20:09:28 +0100

    Add language select

--- src/main/java/edu/kit/typicalc/view/main/HelpDialog.java
+++ src/main/java/edu/kit/typicalc/view/main/HelpDialog.java
@@ -1,11 +1,15 @@
 package edu.kit.typicalc.view.main;
 
+import java.util.Locale;
+
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.H5;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.i18n.LocaleChangeEvent;
 import com.vaadin.flow.i18n.LocaleChangeObserver;
 
@@ -21,9 +25,11 @@ public class HelpDialog extends Dialog implements LocaleChangeObserver {
      */
     private static final String HEADING_LAYOUT_ID = "headingLayout";
     private static final String CONTENT_LAYOUT_ID = "contentLayout";
+    private static final String LANGUAGE_SELECT_ID = "languageSelect";
 
     private final H3 heading;
     private final H5 inputSyntax;
+    private final Select<String> languageSelect;
 
     /**
      * Create a new HelpDialog.
@@ -32,7 +38,13 @@ public class HelpDialog extends Dialog implements LocaleChangeObserver {
         final HorizontalLayout headingLayout = new HorizontalLayout();
         heading = new H3(getTranslation("root.operatingHelp"));
         headingLayout.setId(HEADING_LAYOUT_ID);
-        headingLayout.add(heading);
+        languageSelect = new Select<>(getTranslation("root.german"), getTranslation("root.english"));
+        languageSelect.setLabel(getTranslation("root.selectLanguage"));
+        languageSelect.setValue(getTranslation("root." 
+                + UI.getCurrent().getLocale().getDisplayLanguage(Locale.ENGLISH).toLowerCase()));
+        languageSelect.addValueChangeListener(event -> onLaguageChange());
+        languageSelect.setId(LANGUAGE_SELECT_ID);
+        headingLayout.add(heading, languageSelect);
 
         final VerticalLayout contentLayout = new VerticalLayout();
         //TODO inputSyntax wird im inputDialog behandelt --> hier anderer Content
@@ -40,6 +52,16 @@ public class HelpDialog extends Dialog implements LocaleChangeObserver {
         contentLayout.setId(CONTENT_LAYOUT_ID);
         contentLayout.add(inputSyntax);
         add(headingLayout, contentLayout);
+    }
+
+    private void onLaguageChange() {
+        Locale newLocale = Locale.getDefault();
+        if (languageSelect.getValue().equals(getTranslation("root.german"))) {
+            newLocale = Locale.GERMAN;
+        } else if (languageSelect.getValue().equals(getTranslation("root.english"))) {
+            newLocale = Locale.ENGLISH;
+        }
+        UI.getCurrent().setLocale(newLocale);
     }
 
     @Override
