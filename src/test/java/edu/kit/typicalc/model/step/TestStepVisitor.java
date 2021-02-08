addbe90dcfb9ada04a41bbbc9620cda35ae1340a
uogau <uogau@student.kit.edu>
2021-02-08 12:47:52 +0100

    AbsStepDefault test

---
+++ src/test/java/edu/kit/typicalc/model/step/TestStepVisitor.java
@@ -0,0 +1,44 @@
+package edu.kit.typicalc.model.step;
+
+public class TestStepVisitor implements StepVisitor {
+    public String visited = "";
+    @Override
+    public void visit(AbsStepDefault absD) {
+        visited = "AbsDef";
+    }
+
+    @Override
+    public void visit(AbsStepWithLet absL) {
+        visited = "AbsLet";
+    }
+
+    @Override
+    public void visit(AppStepDefault appD) {
+        visited = "AppDef";
+    }
+
+    @Override
+    public void visit(ConstStepDefault constD) {
+        visited = "ConstDef";
+    }
+
+    @Override
+    public void visit(VarStepDefault varD) {
+        visited = "VarDef";
+    }
+
+    @Override
+    public void visit(VarStepWithLet varL) {
+        visited = "VarLet";
+    }
+
+    @Override
+    public void visit(LetStepDefault letD) {
+        visited = "LetDef";
+    }
+
+    @Override
+    public void visit(EmptyStep empty) {
+        visited = "empty";
+    }
+}
