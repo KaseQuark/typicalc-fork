ef071309ecbd1ac30caf26ee499d7cd6f58eadfa
ucrhh <ucrhh@student.kit.edu>
2021-02-11 11:23:11 +0100

    add package-info to LatexCreator package

---
+++ src/main/java/edu/kit/typicalc/view/content/typeinferencecontent/latexcreator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains the needed classes for creating latex code to display all the structures created by the model.
+ */
+@NonNullApi
+package edu.kit.typicalc.view.content.typeinferencecontent.latexcreator;
+
+import org.springframework.lang.NonNullApi;
