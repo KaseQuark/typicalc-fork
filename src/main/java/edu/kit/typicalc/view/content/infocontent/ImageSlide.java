ef81ef948f6b6128a97d12ad6afd59c43f357ff4
Moritz Dieing <63721811+moritzdieing@users.noreply.github.com>
2021-02-19 12:31:23 +0100

    German version of start page

---
+++ src/main/java/edu/kit/typicalc/view/content/infocontent/ImageSlide.java
@@ -0,0 +1,48 @@
+package edu.kit.typicalc.view.content.infocontent;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.flowingcode.vaadin.addons.carousel.Slide;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
+
+@CssImport("./styles/view/image-slide.css")
+public class ImageSlide extends Slide implements LocaleChangeObserver {
+
+    private static final long serialVersionUID = 232255503611054445L;
+    
+    private static final String SLIDE_LAYOUT_ID = "slideLayout";
+    private static final String EXPLANATION_ID = "explanation";
+    
+    private final VerticalLayout slideLayout;
+    private final Span explanation;
+    private final String explanationKey;
+    private Image image;
+    private final String imagePathKey;
+    
+    public ImageSlide(String imgPathKey, String textKey) {
+        // move to css
+        slideLayout = new VerticalLayout();
+        this.imagePathKey = imgPathKey;
+        image = new Image(getTranslation(imgPathKey), StringUtils.EMPTY);
+        explanationKey = textKey;
+        explanation = new Span(getTranslation(explanationKey));
+        explanation.setId(EXPLANATION_ID);
+        slideLayout.add(image, explanation);
+        slideLayout.setId(SLIDE_LAYOUT_ID);
+        add(slideLayout);
+    }
+    
+    @Override
+    public void localeChange(LocaleChangeEvent event) {
+        explanation.setText(getTranslation(explanationKey));
+        slideLayout.remove(image, explanation);
+        image = new Image(getTranslation(imagePathKey), StringUtils.EMPTY);
+        slideLayout.add(image, explanation);
+    }
+
+}
--- src/main/java/edu/kit/typicalc/view/content/infocontent/StartPageView.java
+++ src/main/java/edu/kit/typicalc/view/content/infocontent/StartPageView.java
@@ -1,84 +1,123 @@
 package edu.kit.typicalc.view.content.infocontent;
 
+import com.flowingcode.vaadin.addons.carousel.Carousel;
+import com.flowingcode.vaadin.addons.carousel.Slide;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.orderedlayout.Scroller;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Hr;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.progressbar.ProgressBar;
+import com.vaadin.flow.i18n.LocaleChangeEvent;
+import com.vaadin.flow.i18n.LocaleChangeObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import edu.kit.typicalc.view.content.ControlPanel;
 import edu.kit.typicalc.view.content.ControlPanelView;
-import edu.kit.typicalc.view.content.typeinferencecontent.MathjaxProofTree;
-import edu.kit.typicalc.view.content.typeinferencecontent.MathjaxUnification;
 import edu.kit.typicalc.view.main.MainViewImpl;
 
 @Route(value = "", layout = MainViewImpl.class)
 @PageTitle(MainViewImpl.PAGE_TITLE)
 @JsModule("./src/mathjax-setup.js")
-public class StartPageView extends VerticalLayout implements ControlPanelView {
+@CssImport("./styles/view/start-page.css")
+public class StartPageView extends VerticalLayout implements ControlPanelView, LocaleChangeObserver {
 
-    private static final String ID = "start-page";
-    private static final String SCROLLER_ID = "start-page-scroller";
+    private static final long serialVersionUID = 2502750919087936406L;
+    
+    private static final String HEADING_ID = "startPage-Heading";
+    private static final String H_LINE_ID = "horizontalLine";
+    private static final String INTRODUCTION_ID = "introduction";
+    private static final String LINK_CONTAINER_ID = "linkContainer";
+    private static final String SLIDE_PROGRESS_ID = "slideProgress";
+    private static final String CONTROL_PANEL_ID = "controlPanel";
+    private static final String START_PAGE_ID = "startPage";
+    private static final String SLIDE_SHOW_ID = "slideShow";
 
-    private final Div content;
+    private static final String DOT = ".";
+    
     private final ControlPanel controlPanel;
-    MathjaxProofTree tree;
-    MathjaxUnification unification;
-
+    private final Span introduction;
+    private final Carousel slideShow;
+    private final ProgressBar slideProgress;
+    private final Text linkText;
+    private final Anchor link;
+    
     public StartPageView() {
-        // todo implement correctly
-        setId(ID);
-        content = new Div();
         controlPanel = new ControlPanel(this, this);
-        Scroller scroller = new Scroller(content);
-        scroller.setId(SCROLLER_ID);
-        scroller.setScrollDirection(Scroller.ScrollDirection.BOTH);
-        add(scroller, controlPanel);
-        disableControlPanel();
-        createContent();
-
-    }
-
-    private void createContent() {
-        content.add("TODO");
-    }
-
-    private void disableControlPanel() {
-        // todo disable everything
-        controlPanel.setEnabledFirstStep(false);
-        controlPanel.setEnabledLastStep(false);
-        controlPanel.setEnabledNextStep(false);
-        controlPanel.setEnabledPreviousStep(false);
+        controlPanel.setId(CONTROL_PANEL_ID);
         controlPanel.setEnabledShareButton(false);
+        
+        slideShow = createSzenarioCarousel();
+        slideShow.setId(SLIDE_SHOW_ID);
+        
+        H1 heading = new H1(getTranslation("root.typicalc"));
+        heading.setId(HEADING_ID);
+        Hr line1 = new Hr();
+        line1.setId(H_LINE_ID);
+        Hr line2 = new Hr();
+        line2.setId(H_LINE_ID);
+        introduction = new Span(getTranslation("root.slideExp"));
+        introduction.setId(INTRODUCTION_ID);
+        
+        linkText = new Text(getTranslation("root.linkText"));
+        link = new Anchor(getTranslation("root.link"), getTranslation("root.here"));
+        Span linkContainer = new Span(linkText, link, new Text(DOT));
+        linkContainer.setId(LINK_CONTAINER_ID);
+        
+        slideProgress = new ProgressBar(slideShow.getStartPosition(), slideShow.getSlides().length - 1);
+        slideProgress.setId(SLIDE_PROGRESS_ID);
+        
+        add(heading, line1, introduction, linkContainer, slideProgress, slideShow, line2, controlPanel);
+        setId(START_PAGE_ID);
     }
 
-    private int currentStep = 0;
-    private void refreshElements(int currentStep) {
-        unification.showStep(currentStep);
-        tree.showStep(currentStep < tree.getStepCount() ? currentStep : tree.getStepCount() - 1);
+    private Carousel createSzenarioCarousel() {
+        Slide slide1 = new ImageSlide("root.image1", "root.text1");
+        Slide slide2 = new ImageSlide("root.image2", "root.text2");
+        Slide slide3 = new ImageSlide("root.image3", "root.text3");
+        Slide slide4 = new ImageSlide("root.image4", "root.text4");
+        Slide slide5 = new ImageSlide("root.image5", "root.text5");
+        Slide slide6 = new ImageSlide("root.image6", "root.text6");
+        Slide slide7 = new ImageSlide("root.image7", "root.text7");
+        Slide slide8 = new ImageSlide("root.image8", "root.text8");
+        
+        return new Carousel(slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8).withoutNavigation()
+                .withoutSwipe();
     }
-
+    
     @Override
     public void firstStepButton() {
-        currentStep = currentStep > tree.getStepCount() ? tree.getStepCount() - 1 : 0;
-        refreshElements(currentStep);
+        slideShow.movePos(0);
+        slideProgress.setValue(0);
     }
 
     @Override
     public void lastStepButton() {
-        currentStep = currentStep < tree.getStepCount() - 1 ? tree.getStepCount() - 1 : unification.getStepCount() - 1;
-        refreshElements(currentStep);
+        slideShow.movePos(slideShow.getSlides().length - 1);
+        slideProgress.setValue(slideShow.getSlides().length - 1);
     }
 
     @Override
     public void nextStepButton() {
-        currentStep = currentStep < unification.getStepCount() - 1 ? currentStep + 1 : currentStep;
-        refreshElements(currentStep);
+        slideShow.moveNext();
+        slideProgress.setValue((slideProgress.getValue() + 1) % slideShow.getSlides().length);
     }
 
     @Override
     public void previousStepButton() {
-        currentStep = currentStep > 0 ? currentStep - 1 : currentStep;
-        refreshElements(currentStep);
+        slideShow.movePrev();
+        slideProgress.setValue((slideProgress.getValue() - 1 + slideShow.getSlides().length) 
+                % slideShow.getSlides().length);
+    }
+    
+    @Override
+    public void localeChange(LocaleChangeEvent event) {
+        introduction.setText(getTranslation("root.slideExp"));
+        linkText.setText(getTranslation("root.linkText"));
+        link.setHref(getTranslation("root.link"));
+        link.setText(getTranslation("root.here"));
     }
 }
