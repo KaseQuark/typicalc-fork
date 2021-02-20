d6bc8bb9cd5406f8c456cb0bbf6315317125ef51
Moritz Dieing <63721811+moritzdieing@users.noreply.github.com>
2021-02-20 01:01:41 +0100

    package info for content and infocontent

---
+++ src/main/java/edu/kit/typicalc/view/content/infocontent/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package only contains the classes needed to build the start page of the application.
+ * The start page consists of an introduction and a short demonstration of the features of the website.
+ */
+@org.springframework.lang.NonNullFields
+@org.springframework.lang.NonNullApi
+package edu.kit.typicalc.view.content.infocontent;
---
+++ src/main/java/edu/kit/typicalc/view/content/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The content package contains two sub-packages. One package consists of the structures needed to display the
+ * type inference algorithm step by step, the other one contains structures to build the start page of the website.
+ * The classes inside the content package are used by both sub packages.
+ */
+@org.springframework.lang.NonNullFields
+@org.springframework.lang.NonNullApi
+package edu.kit.typicalc.view.content;
