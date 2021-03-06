a176d299ce16e4ab81741e8aa9ce55f0a55445e9
Arne Keller <arne.keller@student.kit.edu>
2021-03-06 13:43:42 +0100

    Only accept ASCII characters in type assumption name

--- src/main/java/edu/kit/typicalc/util/package-info.java
+++ src/main/java/edu/kit/typicalc/util/package-info.java
@@ -1,9 +1,4 @@
 /**
  * The util package contains classes used in all components of the application.
  */
-@NonNullFields
-@NonNullApi
 package edu.kit.typicalc.util;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
