884e82dab1c7f7a33ce7569ff5869d5ab2b8aa6a
Arne Keller <arne.keller@student.kit.edu>
2021-06-15 12:24:36 +0200

    and a python3 package

--- shell.nix
+++ shell.nix
@@ -1,7 +1,12 @@
 with (import <nixpkgs> {});
-let mavenJdk11 = maven.override {
-  jdk = pkgs.jdk11;
-};
+let
+  mavenJdk11 = maven.override {
+    jdk = pkgs.jdk11;
+  };
+  my-python3-packages = python3-packages: with python3-packages; [
+    lxml
+  ];
+  python3-with-my-packages = pkgs.python3.withPackages my-python3-packages;
 in
 mkShell {
   nativeBuildInputs = [
@@ -10,6 +15,6 @@ mkShell {
     mavenJdk11
     nodejs-14_x
     which # required by Vaadin
-    python3
+    python3-with-my-packages
   ];
 }
