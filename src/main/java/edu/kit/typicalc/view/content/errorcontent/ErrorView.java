3477bff512bb59dd0e386fbddc4ffaff8d736d8e
uogau <uogau@student.kit.edu>
2021-06-29 12:07:19 +0200

    Fixed off by one error for english error message

--- src/main/java/edu/kit/typicalc/view/content/errorcontent/ErrorView.java
+++ src/main/java/edu/kit/typicalc/view/content/errorcontent/ErrorView.java
@@ -61,19 +61,20 @@ public class ErrorView extends VerticalLayout implements LocaleChangeObserver {
             case UNEXPECTED_TOKEN:
                 Optional<Token> cause = error.getCause();
                 if (cause.isPresent()) {
-                    additionalInformation.add(new Span(new Pre(getTranslation("root.term") + term)));
+                    additionalInformation.add(new Span(new Pre(getTranslation("root.termForError") + term)));
                     additionalInformation.add(
-                            new Span(new Pre(" ".repeat(Math.max(getTranslation("root.term").length(),
-                                    cause.get().getPos() + "root.term".length()))
+                            new Span(new Pre(" ".repeat(Math.max(getTranslation("root.termForError").length(),
+                                    cause.get().getPos() + getTranslation("root.termForError").length()))
                             + "^ " + getTranslation("root.wrongCharacter") + cause.get().getText())));
                 }
                 break;
             case UNEXPECTED_CHARACTER:
                 char c = error.getWrongCharacter();
                 if (c != '\0') {
-                    additionalInformation.add(new Span(new Pre(getTranslation("root.term") + term)));
-                    additionalInformation.add(new Span(new Pre(" ".repeat(Math.max(getTranslation("root.term").length(),
-                            error.getPosition() + "root.term".length()))
+                    additionalInformation.add(new Span(new Pre(getTranslation("root.termForError") + term)));
+                    additionalInformation.add(new Span(
+                            new Pre(" ".repeat(Math.max(getTranslation("root.termForError").length(),
+                            error.getPosition() + getTranslation("root.termForError").length()))
                             + "^ " + getTranslation("root.wrongCharacter") + c)));
                 } else {
                     return summary;
