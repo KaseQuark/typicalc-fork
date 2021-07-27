6df7f0f44305a732a66b8624c70647a74451460b
Arne Keller <arne.keller@student.kit.edu>
2021-07-27 13:08:38 +0200

    Fix tooltip display in Google Chrome

--- frontend/src/mathjax-proof-tree.ts
+++ frontend/src/mathjax-proof-tree.ts
@@ -81,7 +81,8 @@ class MathjaxProofTree extends MathjaxAdapter {
                             margin: 0 !important;\
                         }\
                         .typicalc-type, g[semantics='bspr_prooflabel:left'] {\
-                            stroke: transparent; stroke-width: 600px; pointer-events: all;\
+                            /* cross-browser-compatibility: Chrome does not support the stroke trick, but instead bounding-box (which is not supported by Firefox..) */\
+                            stroke: transparent; stroke-width: 600px; pointer-events: all; pointer-events: bounding-box;\
                         }\
                         #typicalc-definition-abs, #typicalc-definition-abs-let, #typicalc-definition-app,\
                         #typicalc-definition-const, #typicalc-definition-var, #typicalc-definition-var-let, #typicalc-definition-let {\
