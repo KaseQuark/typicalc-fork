fbb80ae3ad672bd5c1cab8b370e82de810047667
Arne Keller <arne.keller@student.kit.edu>
2021-08-30 14:55:16 +0200

    Scroll to explanation text if needed
    fixes #26

--- frontend/src/mathjax-explanation.ts
+++ frontend/src/mathjax-explanation.ts
@@ -41,19 +41,11 @@ class MathjaxExplanation extends MathjaxAdapter {
 		if (el) {
 			this.lastStepShown = n;
 			// scroll to element if needed
-			/* TODO: this works, but Vaadin still resets the scroll position when updating the unification text...
 			const hostEl = this.shadowRoot!.host as HTMLElement;
 			const dy = el.offsetTop - hostEl.offsetTop - hostEl.scrollTop;
 			if (dy > hostEl.offsetHeight || dy < 0) {
-				console.log("doing it!");
-				console.log(dy);
-				console.log(hostEl.scrollTop);
-				console.log(hostEl.offsetTop);
-				console.log(el.offsetTop);
-				console.log("scrolling by " + (-hostEl.scrollTop + el.offsetTop - hostEl.offsetTop));
 				hostEl.scrollBy(0, -hostEl.scrollTop + el.offsetTop - hostEl.offsetTop);
 			}
-			 */
 			el.style.opacity = "1.0";
 		}
 	}
